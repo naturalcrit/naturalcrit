948f719ff398108d15b00d64a3ffe6128ceab861
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2025-11-21 12:10:01 +0100

    lets log this name thingy

--- scripts/build.js
+++ scripts/build.js
@@ -9,6 +9,7 @@ Promise.resolve()
 	.then(()=>steps.clean())
 	.then(()=>steps.libs(Proj.libs))
 	.then(()=>Promise.all(_.map(Proj.apps, (path, name)=>
+		console.log(name),
 		steps.jsx(name, path, {libs : Proj.libs, shared : Proj.shared})
 			.then((deps)=>steps.less(name, {shared : Proj.shared}, deps))
 	)))
