f4c7f4e5bb478c7dd964033c3120cc11222661b2
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2025-11-21 12:12:31 +0100

    lets log it better

--- scripts/build.js
+++ scripts/build.js
@@ -8,11 +8,11 @@ const Proj  = require('./project.json');
 Promise.resolve()
 	.then(()=>steps.clean())
 	.then(()=>steps.libs(Proj.libs))
-	.then(()=>Promise.all(_.map(Proj.apps, (path, name)=>
-		console.log('name: ',name),
-		steps.jsx(name, path, {libs : Proj.libs, shared : Proj.shared})
-			.then((deps)=>steps.less(name, {shared : Proj.shared}, deps))
-	)))
+	.then(() => Promise.all(_.map(Proj.apps, (path, name) => {
+    console.log('Building bundle for app name:', name); // <- add this
+    return steps.jsx(name, path, {libs: Proj.libs, shared: Proj.shared})
+        .then((deps) => steps.less(name, {shared: Proj.shared}, deps));
+})))
 	.then(()=>steps.assets(Proj.assets, ['./client', './shared']))
 	.then(()=>console.timeEnd(label))
 	.catch((err)=>console.error(err));
