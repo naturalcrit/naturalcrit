4b5b1bd4a4b65c25ae5e363a77412267f57de74d
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2025-11-22 18:01:34 +0100

    Revert "log every step of the build.js"
    This reverts commit 757f8fff79e9305dfdf1c1d00875e45ed3962460.

--- scripts/build.js
+++ scripts/build.js
@@ -5,63 +5,14 @@ const _     = require('lodash');
 const steps = require('./steps');
 const Proj  = require('./project.json');
 
-function logStep(name) {
-	console.log(`==> Starting: ${name}`);
-}
-
-function logDone(name) {
-	console.log(`✔ Done: ${name}`);
-}
-
 Promise.resolve()
-
-	// CLEAN
-	.then(() => {
-		logStep('clean()');
-		return steps.clean()
-			.then(() => logDone('clean()'));
-	})
-
-	// LIBS
-	.then(() => {
-		logStep('libs()');
-		return steps.libs(Proj.libs)
-			.then(() => logDone('libs()'));
-	})
-
-	// APPS (jsx + less)
-	.then(() => {
-		logStep('apps (jsx + less)');
-		return Promise.all(
-			_.map(Proj.apps, (path, name) => {
-				console.log(`→ Processing app: ${name}`);
-
-				return steps.jsx(name, path, { libs: Proj.libs, shared: Proj.shared })
-					.then((deps) => {
-						console.log(`   JSX complete: ${name}`);
-						return steps.less(name, { shared: Proj.shared }, deps);
-					})
-					.then(() => {
-						console.log(`   LESS complete: ${name}`);
-					});
-			})
-		).then(() => logDone('apps (jsx + less)'));
-	})
-
-	// ASSETS
-	.then(() => {
-		logStep('assets()');
-		return steps.assets(Proj.assets, ['./client', './shared'])
-			.then(() => logDone('assets()'));
-	})
-
-	// END
-	.then(() => {
-		console.timeEnd(label);
-		console.log('✔ BUILD COMPLETE');
-	})
-
-	.catch((err) => {
-		console.error('❌ BUILD FAILED');
-		console.error(err);
-	});
+	.then(()=>steps.clean())
+	.then(()=>steps.libs(Proj.libs))
+	.then(() => Promise.all(_.map(Proj.apps, (path, name) => {
+    console.log('Building bundle for app name:', name); // <- add this
+    return steps.jsx(name, path, {libs: Proj.libs, shared: Proj.shared})
+        .then((deps) => steps.less(name, {shared: Proj.shared}, deps));
+})))
+	.then(()=>steps.assets(Proj.assets, ['./client', './shared']))
+	.then(()=>console.timeEnd(label))
+	.catch((err)=>console.error(err));
