95df0fa12c99b031e2fec7c20dbab64c5a92e8be
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2025-11-23 18:25:20 +0100

    is cookie not being created?

--- client/naturalcrit/account.actions.js
+++ client/naturalcrit/account.actions.js
@@ -93,7 +93,7 @@ const AccountActions = {
 
 	createSession: (token) => {
 		const domain = window.domain === '.local.naturalcrit.com' ? 'localhost' : window.domain;
-		console.log('creatin cookie', domain);
+		console.log('creatin cookie', `nc_session=${token}; max-age=${60 * 60 * 24 * 365}; path=/; samesite=lax;domain=${domain}`);
 		document.cookie = `nc_session=${token}; max-age=${60 * 60 * 24 * 365}; path=/; samesite=lax;domain=${domain}`;
 	},
 
